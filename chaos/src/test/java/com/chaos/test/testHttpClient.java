ad43108f522375b1e54e2c1d33feb795a8a2de17
username <email>
2018-10-16 13:41:25 +0800

    20181016

---
+++ chaos/src/test/java/com/chaos/test/testHttpClient.java
@@ -0,0 +1,59 @@
+package com.chaos.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class testHttpClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		String httpurl="http://192.168.0.160:8888/PrintService4?id=9521";
+		
+		HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedReader br = null;
+        String result = null;// 返回结果字符串
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(httpurl);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            connection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            connection.setConnectTimeout(15000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            connection.setReadTimeout(60000);
+            // 发送请求
+            connection.connect();
+            // 通过connection连接，获取输入流
+            if (connection.getResponseCode() == 200) {
+                is = connection.getInputStream();
+                // 封装输入流is，并指定字符集
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                // 存放数据
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+         finally {        
+           br.close();
+           is.close(); 
+            connection.disconnect();// 关闭远程连接
+        }
+	}
+
+}
